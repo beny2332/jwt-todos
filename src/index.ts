257a3d4a6398a67539925f152c8c778ea12ca3e8
BinyaminYacovson <bj57577@gmail.com>
2024-09-30 11:18:32 +0300

    Add controller files and setup routes in index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express"
+import cookieParser from "cookie-parser"
+import "dotenv/config"
+import authController from "./controllers/authController"
+import todoController from "./controllers/todoCotroller"
+import userController from "./controllers/userController"
+
+const app: Express = express()
+
+app.use(express.json()) // to get the req.body
+app.use(cookieParser()) // to get the req.cookie(s)
+app.use("/auth", authController)
+app.use("/todos", todoController)
+app.use("/users", userController)
+app.listen(process.env.PORT, () =>
+  console.log(
+    `Server is up and running, feel free to visit at http://localhost:${process.env.PORT}`
+  )
+)
