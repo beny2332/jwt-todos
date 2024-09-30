9e295afafe06eac6f713ee0b8580b2225dc68c24
BinyaminYacovson <bj57577@gmail.com>
2024-09-30 11:45:58 +0300

    Add initial todos for Johnny

--- src/data/data.ts
+++ src/data/data.ts
@@ -12,7 +12,7 @@ export const todos: Todo[] = [];
 
     const laundry: Todo = new Todo("Fold some clean laundry", johnny.id)
     const dishes: Todo = new Todo("Wash the dirty dishes before dinner", johnny.id)
-    
+
     users.push(johnny)
     todos.push(laundry, dishes)
 })()
